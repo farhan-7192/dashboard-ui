eca235122cc44d13b409c7b25c0536eb2a338272
Farhan <farhan@aptlylabs.com>
2026-03-08 18:18:46 +0530

    UI change

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,12 @@ import CampaignsList from "./components/CampaignsList";
 
 export default function App() {
   return (
-    <SidebarProvider
-      className={`
-        bg-white
-        overflow-hidden
-        flex
-        h-screen
-        w-full
-      `}
-    >
+    <SidebarProvider className="bg-white overflow-hidden flex h-screen w-full">
       <Sidebar />
 
-      <div
-        className={`
-          flex-1
-          flex
-          flex-col
-          min-w-0
-          bg-white
-          border-l
-          border-slate-200
-        `}
-      >
+      <div className="flex-1 flex flex-col min-w-0 bg-white border-l border-slate-200">
         <TopHeader />
-        <main
-          className={`
-            flex-1
-            overflow-y-auto
-            px-8
-            py-6
-          `}
-        >
+        <main className="flex-1 overflow-y-auto px-8 py-6">
           <AdBanner />
           <Filter />
           <Toggle />
